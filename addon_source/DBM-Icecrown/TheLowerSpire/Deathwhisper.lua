715cbcf60beaf30794d0d99cd9b2549a6872ca0f
sanctum32 <andriuspel@gmail.com>
2022-06-06 04:14:49 +0300

    Dungeons/ICC: addition to previous commit

--- addon_source/DBM-Icecrown/TheLowerSpire/Deathwhisper.lua
+++ addon_source/DBM-Icecrown/TheLowerSpire/Deathwhisper.lua
@@ -4,7 +4,6 @@ local L     = mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision: 4411 $"):sub(12, -3))
 mod:SetCreatureID(36855)
 mod:SetUsedIcons(4, 5, 6, 7, 8)
-mod:RegisterCombat("yell", L.YellPull)
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
