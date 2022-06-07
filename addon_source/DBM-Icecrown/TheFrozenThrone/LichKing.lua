fd5ab6474e91e9dfd85eaf94e707caaaf5b2e6e4
sanctum32 <andriuspel@gmail.com>
2022-06-07 10:59:33 +0300

    Dungeons/ICC: reverted last change for LK combat timer, seems 70 seconds been OK

--- addon_source/DBM-Icecrown/TheFrozenThrone/LichKing.lua
+++ addon_source/DBM-Icecrown/TheFrozenThrone/LichKing.lua
@@ -66,7 +66,7 @@ local specWarnHarvestSouls    = mod:NewSpecialWarningSpell(74297) --Heroic Abili
 local specWarnValkyrLow        = mod:NewSpecialWarning("SpecWarnValkyrLow")
 local specWarnIcePulse          = mod:NewSpecialWarningMove(69091) -- Transition (Ice Sphere)
 
-local timerCombatStart        = mod:NewTimer(72, "TimerCombatStart", 2457)
+local timerCombatStart        = mod:NewTimer(70, "TimerCombatStart", 2457)
 local timerPhaseTransition    = mod:NewTimer(62, "PhaseTransition", 72262)
 local timerSoulreaper         = mod:NewTargetTimer(5.1, 73797, nil, mod:IsTank() or mod:IsHealer())
 local timerSoulreaperCD         = mod:NewCDTimer(33, 73797, nil, mod:IsTank() or mod:IsHealer())
